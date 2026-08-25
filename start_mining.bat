50b814254469113751da5a711100e8c74431b0a0
Kurtcoin Core Developer <320931476+kurtcoin@users.noreply.github.com>
2026-08-25 18:04:36 +0300

    feat(release): include start_mining.bat launcher in repository

---
+++ start_mining.bat
@@ -0,0 +1,13 @@
+@echo off
+title Kurtcoin (KURT) - Canli Madenci ve Blokzincir Dugumu
+color 0b
+echo ==================================================================
+echo   ?? KURTCOIN (KURT) - RESMI KATMAN-1 MADENCI MOTORU
+echo   Hedef Cuzdan: 14FTsfBTzSBP9Zm9W65W1YNUEYhXJ3t6PA
+echo   Blok Odulu: 50 KURT
+echo   Blok Suresi: 1 Dakika
+echo ==================================================================
+echo.
+cd /d "%~dp0"
+node kurtcoin_node.js
+pause
